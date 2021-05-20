172a716a3730f62b2037e0539285189d9e50fedf
DFLops <danielfarialopes@gmail.com>
2021-05-20 17:11:51 -0300

    Update formulario1script.js
    encurtado o código; mesma função

--- formulario1script.js
+++ formulario1script.js
@@ -1,14 +1,7 @@
 function insertGender() {
     if (document.getElementById('genderOther').checked) {
         document.getElementById('genderOtherInput').disabled = false;
-    }
-    if (document.getElementById('man').checked) {
-        document.getElementById('genderOtherInput').disabled = true;
-    }
-    if (document.getElementById('woman').checked) {
-        document.getElementById('genderOtherInput').disabled = true;
-    }
-    if (document.getElementById('nonBinary').checked) {
+    } else {
         document.getElementById('genderOtherInput').disabled = true;
     }
 }
